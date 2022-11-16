eb205f59cad6f2f408dc6927fe7a113cbee45d4a
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-11-16 07:17:03 -0300

    Refatorando o código

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,26 @@
-import Head from "../../components/head";
+import { HeadSection, Container, EspecialOffers } from "./style";
+
+import Head1 from "../../assets/head/head1.png";
+
+import Head2 from "../../assets/head/head2.png";
 
 export default function Home() {
   return (
     <>
-      <Head />
+      <HeadSection>
+        <Container>
+          <div>
+            <h2>Women</h2>
+            <img src={Head1} alt="Head 1" />
+            <p>Dioptric | Sunglasses</p>
+          </div>
+          <div>
+            <h2>Men</h2>
+            <img src={Head2} alt="Head 1" />
+            <p>Dioptric | Sunglasses</p>
+          </div>
+        </Container>
+      </HeadSection>
     </>
-  )
+  );
 }
---
+++ src/pages/home/style.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+
+export const HeadSection = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.backgroundColor};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 5rem;
+  }
+
+  h2 {
+    font-size: 4rem;
+    font-weight: 500;
+    letter-spacing: 8px;
+    line-height: 24px;
+    text-transform: uppercase;
+    color: #ffffff;
+    text-align: center;
+    padding-bottom: 1rem;
+  }
+
+  img {
+    width: 70%;
+  }
+
+  p {
+    color: ${({ theme }) => theme.color.primaryColor};
+    font-size: 1.125rem;
+    font-weight: 500;
+    letter-spacing: 2px;
+    line-height: 24px;
+    text-transform: uppercase;
+  }
+
+  @media screen and (min-width: 767px) {
+    max-width: ${({ theme }) => theme.container.maxWidth};
+    margin: auto;
+    padding-top: 10rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+
+    div {
+      padding-bottom: none;
+    }
+
+    h2 {
+      font-size: 5rem;
+      padding-bottom: unset;
+    }
+
+    img {
+      width: 90%;
+      padding: 2rem 0 5.43rem 0;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    img {
+      width: 450px;
+    }
+  }
+`;
+
+export const EspecialOffers = styled.section`
+  width: 100%;
+`;
