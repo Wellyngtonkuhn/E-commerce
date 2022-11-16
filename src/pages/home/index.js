a4cdbf0cef93d7b15ff2c8a148b38188a90e7d87
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-11-16 09:37:35 -0300

    Especial offers section pronta

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,26 +1,77 @@
-import { HeadSection, Container, EspecialOffers } from "./style";
+import {
+  MainHome,
+  HeadSection,
+  Container,
+  HeadContent,
+  EspecialOffersSection,
+  EspecialOffersContent,
+} from "./style";
 
 import Head1 from "../../assets/head/head1.png";
-
 import Head2 from "../../assets/head/head2.png";
+import OfferImg1 from "../../assets/specialOffers/offer1.png";
+import OfferImg2 from "../../assets/specialOffers/offer2.png";
+import FreeShipping from "../../assets/specialOffers/free-shipping.png";
+import ReturnImg from "../../assets/specialOffers/return.png";
 
 export default function Home() {
   return (
     <>
-      <HeadSection>
-        <Container>
-          <div>
-            <h2>Women</h2>
-            <img src={Head1} alt="Head 1" />
-            <p>Dioptric | Sunglasses</p>
-          </div>
-          <div>
-            <h2>Men</h2>
-            <img src={Head2} alt="Head 1" />
-            <p>Dioptric | Sunglasses</p>
-          </div>
-        </Container>
-      </HeadSection>
+      <MainHome>
+        <HeadSection>
+          <Container>
+            <HeadContent>
+              <div>
+                <h2>Women</h2>
+                <img src={Head1} alt="Head 1" />
+                <p>Dioptric | Sunglasses</p>
+              </div>
+              <div>
+                <h2>Men</h2>
+                <img src={Head2} alt="Head 1" />
+                <p>Dioptric | Sunglasses</p>
+              </div>
+            </HeadContent>
+          </Container>
+        </HeadSection>
+
+        <EspecialOffersSection>
+          <Container>
+            <EspecialOffersContent>
+              <div className="firstColumn">
+                <div className="offerImgContent">
+                  <img src={OfferImg1} alt="imagem1" />
+                  <div>
+                    <h3>Fusce sit amet est quam dolor sit ametis</h3>
+                    <button>Learn More </button>
+                  </div>
+                </div>
+
+                <div className="infoDelivery">
+                  <div>
+                    <img src={FreeShipping} alt="free-shipping" />
+                    <h3>Free shipping</h3>
+                    <p>worldwide</p>
+                  </div>
+                  <div>
+                    <img src={ReturnImg} alt="free-return" />
+                    <h3>14 days free return</h3>
+                    <p>worldwide</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="offerImgContent secondColumn">
+                <img src={OfferImg2} alt="imagem2" />
+                <div className="offerContent">
+                  <h3>Fusce sit amet est quam dolor sit ametis</h3>
+                  <button>Learn More </button>
+                </div>
+              </div>
+            </EspecialOffersContent>
+          </Container>
+        </EspecialOffersSection>
+      </MainHome>
     </>
   );
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,14 +1,30 @@
 import styled from "styled-components";
 
-export const HeadSection = styled.section`
+export const MainHome = styled.main`
   width: 100%;
   min-height: 100vh;
+`;
+
+export const HeadSection = styled.section`
+  width: 100%;
+  height: 100%;
   background-color: ${({ theme }) => theme.backgroundColor};
 `;
 
 export const Container = styled.div`
+  @media screen and (min-width: 767px) {
+    max-width: ${({ theme }) => theme.container.maxWidth};
+    margin: auto;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+export const HeadContent = styled.div`
   display: flex;
   flex-direction: column;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
 
   div {
     display: flex;
@@ -42,12 +58,6 @@ export const Container = styled.div`
   }
 
   @media screen and (min-width: 767px) {
-    max-width: ${({ theme }) => theme.container.maxWidth};
-    margin: auto;
-    padding-top: 10rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
-
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -74,6 +84,133 @@ export const Container = styled.div`
   }
 `;
 
-export const EspecialOffers = styled.section`
+export const EspecialOffersSection = styled.section`
   width: 100%;
+  height: 100%;
+  margin-top: -7rem;
+  margin-bottom: 2rem;
+`;
+
+export const EspecialOffersContent = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    padding: 0.5rem;
+  }
+  img {
+    width: 100%;
+  }
+
+  .offerImgContent {
+    position: relative;
+
+    div {
+      width: 60%;
+      position: absolute;
+      bottom: 0;
+      right: 5px;
+      text-align: center;
+
+      h3 {
+        font-size: 1.3rem;
+        color: #ffffff;
+        font-weight: 500;
+      }
+
+      button {
+        margin: 1.2rem 0;
+        padding: 0.5rem 1rem;
+        background-color: transparent;
+        color: #ffffff;
+        border: 1px solid #fff;
+
+        font-size: 1.125rem;
+        letter-spacing: 2px;
+        line-height: 24px;
+        text-transform: uppercase;
+        font-weight: 500;
+      }
+    }
+    .offerContent {
+      width: 75%;
+      right: unset;
+    }
+  }
+
+  .secondColumn {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .infoDelivery {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    border: 1px solid ${({ theme }) => theme.color.primaryColor};
+
+    div {
+      text-align: center;
+      color: ${({ theme }) => theme.color.primaryColor};
+      width: 50%;
+    }
+
+    div:first-child {
+      border-right: 1px solid ${({ theme }) => theme.color.primaryColor};
+    }
+
+    img {
+      width: 2rem;
+    }
+
+    h3 {
+      font-size: 1.2rem;
+      letter-spacing: 2px;
+      text-transform: uppercase;
+      font-weight: 700;
+    }
+
+    p {
+      font-size: 1.1rem;
+      line-height: 36px;
+      color: #b2b2b2;
+      font-weight: bold;
+      font-family: "Myriad Pro";
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+
+    .firstColumn {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .offerImgContent {
+      div {
+        h3 {
+          font-size: 1.875rem;
+        }
+        button {
+          font-size: 1.5rem;
+          cursor: pointer;
+        }
+      }
+    }
+    .infoDelivery {
+      div h3 {
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .infoDelivery {
+      padding: 2rem;
+    }
+  }
 `;
