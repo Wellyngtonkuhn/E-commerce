29b8f80a9bcb68866ceb6014472f53304d1a44e5
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-11-17 08:26:51 -0300

    Ajustando o responsivo

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -10,6 +10,7 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  flex-wrap: wrap;
 
   padding: 2rem 1rem;
 
@@ -26,11 +27,15 @@ export const Content = styled.div`
   @media screen and (min-width: 768px) {
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
+    justify-content: center;
 
     div {
       flex-direction: row;
       gap: 1rem;
     }
+  }
+
+  @media screen and (min-width: 1024px) {
+    justify-content: space-between;
   }
 `;
